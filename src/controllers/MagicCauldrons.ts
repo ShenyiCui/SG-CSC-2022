d1bb33908615a4549c0cfc8485bcabb96d27b863
ShenyiCui <shenyicui@outlook.com>
2022-09-17 16:57:31 +0800

    fix: fix incorrect part1 logic

--- src/controllers/MagicCauldrons.ts
+++ src/controllers/MagicCauldrons.ts
@@ -28,29 +28,50 @@ type Output = {
   part4: number;
 };
 
-const partOne = (input: Part1): number => {
-  const {flow_rate, time, row_number, col_number} = input;
-  const totalSoup = flow_rate * time;
-  const numOfBowlsAbove = row_number * ((1 + row_number) / 2);
-  const soupAtMyLevel = totalSoup - numOfBowlsAbove * 100;
-
-  if (soupAtMyLevel <= 0) {
-    return 0;
-  }
-
-  const soupFlow = soupAtMyLevel / (row_number + 1);
-  return getCurrentSoupLevel(soupFlow, row_number, col_number);
+const roundToTwo = (num: number) => {
+  var m = Number((Math.abs(num) * 100).toPrecision(15));
+  return (Math.round(m) / 100) * Math.sign(num);
 };
 
-const getCurrentSoupLevel = (
-  soupFlow: number,
-  row: number,
-  col: number
-): number => {
-  if (col === 0 || col === row + 1) {
-    return soupFlow;
-  }
-  return soupFlow * 2;
+const partOne = (input: Part1): number => {
+  const {flow_rate, time, row_number, col_number} = input;
+  const memory = new Map<string, number>();
+
+  const findOverflow = (row: number, col: number): number => {
+    if (row == 0 && col == 0) {
+      const overflow = flow_rate * time - 100;
+      return overflow < 0 ? 0 : overflow / 2;
+    }
+    if (col < 0 || col > row) {
+      return 0;
+    }
+    if (memory.has([row, col].toString())) {
+      return memory.get([row, col].toString())!;
+    }
+
+    const currentValue =
+      findOverflow(row - 1, col) + findOverflow(row - 1, col - 1);
+
+    if (currentValue > 100) {
+      const overflow = (currentValue - 100) / 2;
+      memory.set([row, col].toString(), overflow);
+      return overflow;
+    }
+
+    memory.set([row, col].toString(), 0);
+    return 0;
+  };
+
+  const solve = (row: number, col: number): number => {
+    if (row === 0 && col === 0) {
+      const isOverflow = flow_rate * time - 100 > 0;
+      return isOverflow ? 100 : flow_rate * time;
+    }
+    const answer = findOverflow(row - 1, col) + findOverflow(row - 1, col - 1);
+    return answer >= 100 ? 100 : answer;
+  };
+
+  return solve(row_number, col_number);
 };
 
 export default class MagicCauldronsController {
@@ -58,7 +79,7 @@ export default class MagicCauldronsController {
     const input: Item[] = req.body as Item[];
     const output: Output[] = input.map(parts => {
       return {
-        part1: +partOne(parts.part1).toFixed(2),
+        part1: roundToTwo(partOne(parts.part1)),
         part2: 0,
         part3: 0,
         part4: 0,
