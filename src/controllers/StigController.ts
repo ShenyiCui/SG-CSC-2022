931912048a2e53998bbf5e741d5864372013d389
ShenyiCui <shenyicui@outlook.com>
2022-09-17 20:26:57 +0800

    feat: implement logic for Stig Warmup

--- src/controllers/StigController.ts
+++ src/controllers/StigController.ts
@@ -15,7 +15,7 @@ type Output = {
   q: number;
 };
 
-function gcd(x: number, y: number) {
+function gcdTwoNumbers(x: number, y: number) {
   x = Math.abs(x);
   y = Math.abs(y);
   while (y) {
@@ -26,9 +26,53 @@ function gcd(x: number, y: number) {
   return x;
 }
 
+const getPandQWarmUp = (currentInput: Input): Output => {
+  let q = currentInput.maxRating;
+  let p = 0;
+  for (let i = 1; i <= currentInput.maxRating; i++) {
+    const range = new Array<number>(currentInput.maxRating).fill(0);
+    for (let j = 0; j < currentInput.questions.length; j++) {
+      const currentQuestion = currentInput.questions[j];
+      if (i < currentQuestion.lower || i >= currentQuestion.higher) {
+        range[currentQuestion.lower - 1] += 1;
+        range[currentQuestion.higher - 1] += -1;
+        continue;
+      }
+      range[currentQuestion.higher - 1] += 1;
+      range[0] += 1;
+      range[currentQuestion.lower - 1] += -1;
+    }
+    const prefixSum = [...range];
+    for (let j = 1; j < prefixSum.length; j++) {
+      prefixSum[j] = prefixSum[j - 1] + prefixSum[j];
+    }
+    // console.log('range', range);
+    // console.log('prefix', prefixSum);
+    for (let j = 0; j < prefixSum.length; j++) {
+      if (prefixSum[j] === 0) {
+        if (j + 1 === i) {
+          p++;
+        }
+        break;
+      }
+    }
+  }
+
+  const gcd = gcdTwoNumbers(q, p);
+  return {
+    p: p / gcd,
+    q: q / gcd,
+  };
+};
+
 export default class StigController {
   public warmUp(req: Request, res: Response, next: NextFunction) {
-    res.json({message: 'TypeScript + Express Server'});
+    const input: Input[] = req.body as Input[];
+    const output: Output[] = input.map(currentInput =>
+      getPandQWarmUp(currentInput)
+    );
+
+    res.json(output);
   }
 
   public full(req: Request, res: Response, next: NextFunction) {
