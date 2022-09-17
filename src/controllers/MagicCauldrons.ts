900f4df6473b41e8aeb3e032622355edce22ff7d
ShenyiCui <shenyicui@outlook.com>
2022-09-17 17:19:00 +0800

    feat: add part2 magiccauldron

--- src/controllers/MagicCauldrons.ts
+++ src/controllers/MagicCauldrons.ts
@@ -74,13 +74,82 @@ const partOne = (input: Part1): number => {
   return solve(row_number, col_number);
 };
 
+const partTwo = (input: Part2): number => {
+  const {flow_rate, amount_of_soup, row_number, col_number} = input;
+
+  const findOverflow = (
+    row: number,
+    col: number,
+    time: number,
+    memory: Map<string, number>
+  ): number => {
+    if (row == 0 && col == 0) {
+      const overflow = flow_rate * time - 100;
+      return overflow < 0 ? 0 : overflow / 2;
+    }
+    if (col < 0 || col > row) {
+      return 0;
+    }
+    if (memory.has([row, col].toString())) {
+      return memory.get([row, col].toString())!;
+    }
+
+    const currentValue =
+      findOverflow(row - 1, col, time, memory) +
+      findOverflow(row - 1, col - 1, time, memory);
+
+    if (currentValue > 100) {
+      const overflow = (currentValue - 100) / 2;
+      memory.set([row, col].toString(), overflow);
+      return overflow;
+    }
+
+    memory.set([row, col].toString(), 0);
+    return 0;
+  };
+
+  const solve = (row: number, col: number, time: number): number => {
+    const memory = new Map<string, number>();
+    if (row === 0 && col === 0) {
+      const isOverflow = flow_rate * time - 100 > 0;
+      return isOverflow ? 100 : flow_rate * time;
+    }
+    const answer =
+      findOverflow(row - 1, col, time, memory) +
+      findOverflow(row - 1, col - 1, time, memory);
+    return answer >= 100 ? 100 : answer;
+  };
+
+  let left = 0;
+  let right = 998;
+  let mid = (left + right) / 2;
+  let counter = 0;
+  while (left <= right) {
+    mid = (left + right) / 2;
+    const soupLevel = solve(row_number, col_number, mid);
+    if (soupLevel > amount_of_soup) {
+      right = mid;
+    }
+
+    if (soupLevel < amount_of_soup) {
+      left = mid;
+    }
+
+    if (soupLevel === amount_of_soup || counter >= 100) {
+      return mid;
+    }
+    counter++;
+  }
+  return mid;
+};
+
 export default class MagicCauldronsController {
   public index(req: Request, res: Response, next: NextFunction) {
     const input: Item[] = req.body as Item[];
     const output: Output[] = input.map(parts => {
       return {
         part1: roundToTwo(partOne(parts.part1)),
-        part2: 0,
+        part2: roundToTwo(partTwo(parts.part2)),
         part3: 0,
         part4: 0,
       };
